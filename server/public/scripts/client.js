315ef613b8109e41aafe158819d31a3d788f5b32
Ben Lynch <bdlynch7@gmail.com>
2023-07-21 23:22:27 -0500

    setup jquery framework, setup input capture

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -3,6 +3,33 @@ $(onReady)
 function onReady() {
   console.log('jQuery 🤩');
 
-
+// handlers for buttons
+$('#equals').on('click', handleCapture)
+$('#clear').on('click', handleClear)
 
 } // end onReady
+
+function handleCapture(num1, num2) {
+  // capture inputs to send to server
+  let leftInput = Number($('#input-left').val())
+  let rightInput = Number($('#input-right').val())
+
+  // for POST to server
+  let inputsToSend = [
+    {firstNumber: leftInput},
+    {secondNumber: rightInput} 
+  ]
+  console.log(inputsToSend)
+} // end handleCapture
+
+function handleClear() {
+      // clear text boxes
+      $('#input-left').val('')
+      $('#input-right').val('')
+
+}
+
+function render() {
+console.log('render 👌')
+
+}
--- server/server.js
+++ server/server.js
@@ -1,12 +1,23 @@
 const express = require('express')
 const bodyParser = require('body-parser')
+
 const app = express()
 const PORT = 5000
+
 app.use(bodyParser.urlencoded({extended: true}))
+
 app.use(express.static('server/public'))
 
+// functions to compute (add, subtract, multipy, divide)
+  // send back the OK
 
+// setup POST route for inputs object capture
 
+// GET request after POST to get actual calculation
+
+// recieve input from client.js when '=' is clicked
+
+// history should exist in server after refresh
 
 
 
