cda0bb58b6990cff8188bf3aeac33e57268267c9
Ben Lynch <bdlynch7@gmail.com>
2023-07-22 19:05:19 -0500

    basic server functionality

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,35 +1,87 @@
 $(onReady)
 
+// global variable for answers object
+let answers
+
+
 function onReady() {
   console.log('jQuery 🤩');
-
 // handlers for buttons
-$('#equals').on('click', handleCapture)
-$('#clear').on('click', handleClear)
-
+$('#equals').on('click', getAnswers)
+// $('#clear').on('click', handleClear)
 } // end onReady
 
-function handleCapture(num1, num2) {
-  // capture inputs to send to server
-  let leftInput = Number($('#input-left').val())
-  let rightInput = Number($('#input-right').val())
+let inputsForPost = () => {
+  console.log('in inputsForPost')
+  const inputsForPost = {
+    input: 2,
+    input: 5,
+    operator: minus
+  }
 
-  // for POST to server
-  let inputsToSend = [
-    {firstNumber: leftInput},
-    {secondNumber: rightInput} 
-  ]
-  console.log(inputsToSend)
-} // end handleCapture
+// Ajax to communicate / send data to server.js
+  // url: /incoming
+  $.ajax({
+    method: 'POST', // request type
+    url: '/problems', // route
+    data: inputsForPost // data to send (obj or array)
+  }).then((response) => {
+    console.log('😸 POST:', response) // expect 201
 
-function handleClear() {
-      // clear text boxes
-      $('#input-left').val('')
-      $('#input-right').val('')
-
+    getAnswers()
+    // render () *** setup render function ***
+  
+  }).catch((error) => {
+    console.log('POST error:', error)
+    alert('😿 POST error')
+  })
 }
 
-function render() {
-console.log('render 👌')
 
-}
+let getAnswers = () => {
+  // ajax to GET answers from server
+    // server endpoint: 
+  console.log('inside getAnswers')
+
+  $.ajax({
+    method: 'GET', // how
+    url: '/answers', // where
+  }).then((response) => {
+    console.log('😸 GET:', response)
+    answers = response
+    // render() *** setup render function ***
+  }).catch((error) => {
+    console.log('GET error', error)
+    alert('😿 GET request failed')
+  })
+
+  } // end getAnswers
+
+
+
+
+
+// function handleCapture(num1, num2) {
+//   // capture inputs to send to server
+//   let leftInput = Number($('#input-left').val())
+//   let rightInput = Number($('#input-right').val())
+
+//   // for POST to server
+//   let inputsForPost = [
+//     {firstNumber: leftInput},
+//     {secondNumber: rightInput} 
+//   ]
+//   console.log(inputsForPost)
+// } // end handleCapture
+
+// function handleClear() {
+//       // clear text boxes
+//       $('#input-left').val('')
+//       $('#input-right').val('')
+
+// } // end handleClear
+
+// function render() {
+// console.log('render 👌')
+
+// } // end render
--- server/server.js
+++ server/server.js
@@ -8,21 +8,45 @@ app.use(bodyParser.urlencoded({extended: true}))
 
 app.use(express.static('server/public'))
 
-// functions to compute (add, subtract, multipy, divide)
-  // send back the OK
+let answers = [
+  {input: 3},
+  {input: 8},
+  {operator: '+'}
+]
+
 
 // setup POST route for inputs object capture
+  // history should exist in server after refresh
+app.post('/problems', (req, res) => {
+  console.log('body for problems:', req.body)
+
+  let inputToCalculate = req.body
+  answers.push(inputToCalculate)
+
+  console.log('array of calculations:', answers)
+  res.sendStatus(201)
+})
 
 // GET request after POST to get actual calculation
+app.get('/answers', (req, res) => {
+  console.log('arrived at /answers! answer list:', answers)
+  // server is responding with the answers array
+  res.send(answers)
+  res.sendStatus(200)
+})
 
+
+// port binding
+app.listen(PORT, () => {
+  console.log('🙉 on port:', PORT)
+})
+
+
+// functions to compute (add, subtract, multipy, divide)
+  // send back the OK
 // recieve input from client.js when '=' is clicked
 
-// history should exist in server after refresh
 
 
 
 
-
-app.listen(PORT, () => {
-  console.log('🙉 on port', PORT)
-})
