98725f0a189a91638b6feaccc63c4c39c3d9fd93
Ben Lynch <bdlynch7@gmail.com>
2023-07-23 20:01:11 -0500

    formatted

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,96 +1,89 @@
-$(onReady)
+$(onReady);
 
 // global variable for answers object
-let serverAnswerList = []
-
-
+let serverAnswerList = [];
 
 function onReady() {
-  console.log('jQuery 🤩');
-  getAnswer()
-// handlers for buttons
-$('#equals').on('click', inputPackager)
-$('.operator').on('click', 'button', getOperator)
-$('#clear').on('click', handleClear)
+  console.log("jQuery 🤩");
+  getAnswer();
+  // handlers for buttons
+  $("#equals").on("click", inputPackager);
+  $(".operator").on("click", "button", getOperator);
+  $("#clear").on("click", handleClear);
 } // end onReady
 
-let operatorbutton
+let operatorbutton;
 function getOperator() {
-  console.log($(this).text())
-  operatorbutton = $(this).text()
-  return operatorbutton
+  console.log($(this).text());
+  operatorbutton = $(this).text();
+  return operatorbutton;
 }
 
 function inputPackager() {
-  let inputOne = $('#input-left').val()
-  let inputTwo = $('#input-right').val()
+  let inputOne = $("#input-left").val();
+  let inputTwo = $("#input-right").val();
   let inputPackage = {
     first: inputOne,
     second: inputTwo,
-    operator: operatorbutton
-  }
-  
+    operator: operatorbutton,
+  };
 
-// Ajax to communicate / send data to server.js
+  // Ajax to communicate / send data to server.js
   // url: /incoming
   $.ajax({
-    method: 'POST', // request type
-    url: '/problems', // route
-    data: inputPackage // data to send (obj or array)
-  }).then((response) => {
-    console.log('😸 POST:', response) // expect 201
-
-    getAnswer()
-  
-  }).catch((error) => {
-    console.log('POST error:', error)
-    alert('😿 POST error')
+    method: "POST", // request type
+    url: "/problems", // route
+    data: inputPackage, // data to send (obj or array)
   })
-}
+    .then((response) => {
+      console.log("😸 POST:", response); // expect 201
 
+      getAnswer();
+    })
+    .catch((error) => {
+      console.log("POST error:", error);
+      alert("😿 POST error");
+    });
+}
 
 function getAnswer() {
   // ajax to GET answers from server
-    // server endpoint: /answers
-  console.log('inside getAnswer')
+  // server endpoint: /answers
+  console.log("inside getAnswer");
 
   $.ajax({
-    method: 'GET', // how
-    url: '/answers', // where
-  }).then((response) => {
-    console.log('😸 GET:', response)
-    serverAnswerList = response
-    console.log('answer list:', serverAnswerList)
-    render()
-  }).catch((error) => {
-    console.log('GET error', error)
-    alert('😿 GET request failed')
+    method: "GET", // how
+    url: "/answers", // where
   })
+    .then((response) => {
+      console.log("😸 GET:", response);
+      serverAnswerList = response;
+      console.log("answer list:", serverAnswerList);
+      render();
+    })
+    .catch((error) => {
+      console.log("GET error", error);
+      alert("😿 GET request failed");
+    });
+} // end getAnswer
 
-  } // end getAnswer
-
-  function render() {
-    console.log('render 👌')
-    // empty history
-    $('#history').empty()
-    // loop through array and append 
-    for (let problem of serverAnswerList) {
-      console.log('in for loop, problem:', problem)
-      $('#history').append(`
+function render() {
+  console.log("render 👌");
+  // empty history
+  $("#history").empty();
+  // loop through array and append
+  for (let problem of serverAnswerList) {
+    console.log("in for loop, problem:", problem);
+    $("#history").append(`
         <li>
         ${problem.input1} ${problem.operation} ${problem.input2} = ${problem.solution}
-      
         </li>
-      `)
-    }
-    } // end render
-
+      `);
+  }
+} // end render
 
 function handleClear() {
-      // clear text boxes
-      $('#input-left').val('')
-      $('#input-right').val('')
-
+  // clear text boxes
+  $("#input-left").val("");
+  $("#input-right").val("");
 } // end handleClear
-
-
--- server/server.js
+++ server/server.js
@@ -1,81 +1,70 @@
-const express = require('express')
-const bodyParser = require('body-parser')
+const express = require("express");
+const bodyParser = require("body-parser");
 
-const app = express()
-const PORT = 5000
+const app = express();
+const PORT = 5000;
 
-app.use(bodyParser.urlencoded({extended: true}))
+app.use(bodyParser.urlencoded({ extended: true }));
 
-app.use(express.static('server/public'))
+app.use(express.static("server/public"));
 
-let answerList = []
-let inputToCalculate
-let answerPackage
+let answerList = [];
+let inputToCalculate;
+let answerPackage;
 
 function calculator(inputObject) {
-  console.log('in calculator, object is:', inputObject)
-  let answer = 0
-  let num1 = Number(inputObject.first)
-  let num2 = Number(inputObject.second)
-  console.log('num1:', num1)
-  if (inputObject.operator == '+') {
-   answer = (num1 + num2)
-   console.log('answer is:', answer)
+  console.log("in calculator, object is:", inputObject);
+  let answer = 0;
+  let num1 = Number(inputObject.first);
+  let num2 = Number(inputObject.second);
+  console.log("num1:", num1);
+  if (inputObject.operator == "+") {
+    answer = num1 + num2;
+    console.log("answer is:", answer);
+  } else if (inputObject.operator == "-") {
+    answer = num1 - num2;
+  } else if (inputObject.operator == "*") {
+    answer = num1 * num2;
+  } else if (inputObject.operator == "/") {
+    answer = num1 / num2;
   }
-  else if (inputObject.operator == '-') {
-    answer = num1 - num2
-  }
-  else if (inputObject.operator == '*') {
-    answer = num1 * num2
-  }
-  else if (inputObject.operator == '/') {
-    answer = num1 / num2
-  }
-     console.log('in calculator testing answer', answer)
+  console.log("in calculator testing answer", answer);
   answerPackage = {
     input1: inputObject.first,
     input2: inputObject.second,
     operation: inputObject.operator,
-    solution: answer 
-  }
-  console.log('this is the answer package:', answerPackage)
-  answerList.unshift(answerPackage)
+    solution: answer,
+  };
+  console.log("this is the answer package:", answerPackage);
+  answerList.unshift(answerPackage);
 }
 
 // setup POST route for inputs object capture
-  // history should exist in server after refresh
-app.post('/problems', (req, res) => {
-  console.log('body for problems:', req.body)
+// history should exist in server after refresh
+app.post("/problems", (req, res) => {
+  console.log("body for problems:", req.body);
 
-  inputToCalculate = req.body
-  console.log(inputToCalculate)
-  calculator(inputToCalculate)
+  inputToCalculate = req.body;
+  console.log(inputToCalculate);
+  calculator(inputToCalculate);
 
-  res.sendStatus(201)
-})
-
+  res.sendStatus(201);
+});
 
 // GET request after POST to get actual calculation
-app.get('/answers', (req, res) => {
-  console.log('arrived at /answers! answer list:', answerList)
-  
-  // server is responding with the answerList array
-  res.send(answerList)
-  // res.sendStatus(200) 
-})
+app.get("/answers", (req, res) => {
+  console.log("arrived at /answers! answer list:", answerList);
 
+  // server is responding with the answerList array
+  res.send(answerList);
+  // res.sendStatus(200)
+});
 
 // port binding
 app.listen(PORT, () => {
-  console.log('🙉 on port:', PORT)
-})
-
+  console.log("🙉 on port:", PORT);
+});
 
 // functions to compute (add, subtract, multipy, divide)
-  // send back the OK
+// send back the OK
 // recieve input from client.js when '=' is clicked
-
-
-
-
-
